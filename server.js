b32f5c83e4fdb37e7c2e1e04c31ab31d4e54419a
Cat Kretzschmar <cat.ktz@gmail.com>
2020-06-04 10:45:51 -0400

    small changes for deployment

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+// app.get("/", function(req, res) {
+//   res.json(path.join(__dirname, "public/index.html"));
+// });
+
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
